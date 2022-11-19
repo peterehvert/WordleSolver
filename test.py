9762d8d401bc4238298c5f4400fe1ecf1533dc5b
peterehvert <ehvert.peter@gmail.com>
2022-11-19 19:58:39 +0100

    added function to a definition

--- test.py
+++ test.py
@@ -21,47 +21,49 @@ knownLetters = input('Enter known letters: ')
 eliminatedLetters = input('Enter eliminated letters: ')
 #---------------------------------------------------------#
 
-#---------------------------------------------------------#
-# all the lists and variables needed
-goodGuesses = []
-#---------------------------------------------------------#
 
-#---------------------------------------------------------#
-# loop through every word in the dictionary
-for word in sortedDict:
-    # start word as a valid guess
-    guess = 1
+def CreateAnswers(sortedDict):
+    #---------------------------------------------------------#
+    goodGuesses = []
+    # loop through every word in the dictionary
+    for word in sortedDict:
+        # start word as a valid guess
+        guess = 1
 
-    # loop through every letter in the word
-    for i in range(0, 5):
+        # loop through every letter in the word
+        for i in range(0, 5):
 
-        # if the letter in the word is a locked letter or not known in the solution, continue
-        if ( ( (word[i] == lockedLetters[i]) or (lockedLetters[i] == '-') ) ):
+            # if the letter in the word is a locked letter or not known in the solution, continue
+            if ( ( (word[i] == lockedLetters[i]) or (lockedLetters[i] == '-') ) ):
 
-            # if the letter is in the list of eliminated letters, disqualify the word
-            if ( word[i] in eliminatedLetters):
-                guess = 0
-                break
+                # if the letter is in the list of eliminated letters, disqualify the word
+                if ( word[i] in eliminatedLetters):
+                    guess = 0
+                    break
+
+                else:
+                    # loop through every known letter
+                    for knownLetter in knownLetters:
+                        #if the word does not have one of the known letters in it, disqualify the word
+                        if knownLetter not in word:
+                            guess = 0
+                            break
+                        
+                        else:
+                            guess = 1
 
             else:
-                # loop through every known letter
-                for knownLetter in knownLetters:
-                    #if the word does not have one of the known letters in it, disqualify the word
-                    if knownLetter not in word:
-                        guess = 0
-                        break
-                    
-                    else:
-                        guess = 1
-
-        else:
-            guess = 0
-            break 
-    
-    if guess == 1:
-        goodGuesses.append(word)
+                guess = 0
+                break 
+        
+        if guess == 1:
+            goodGuesses.append(word)
 
 
-# this is a test commit
+    # this is a test commit
 
-print (goodGuesses)
+    print (goodGuesses)
+
+    return goodGuesses
+
+testGuesses = CreateAnswers(sortedDict)
